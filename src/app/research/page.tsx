ff42fbf93c9be33e689817050d7ecae90bc3cba1
JHaaa <543282901@qq.com>
2024-11-11 10:39:30 +0800

    Update website content and layout

--- src/app/research/page.tsx
+++ src/app/research/page.tsx
@@ -9,14 +9,14 @@ const ResearchPage = () => {
       <h3 className={styles.header}>Working Papers</h3>
       <ul className={styles.customList}>
         <li>
-          Jiang, Hang, Jin, Chen, & Yang, Luyi. (2024). 
+          Jiang, Hang, Jin, Chen, &amp; Yang, Luyi. (2024). 
           <a className={styles.link} href="https://papers.ssrn.com/sol3/papers.cfm?abstract_id=4715135" target="_blank" rel="noopener noreferrer"> Revisiting the First-Order Approach to Principal-Agent Problems.
           </a>
-          <br /> &nbsp;&nbsp;&nbsp;— Accepted into the 25th ACM Conference on Economics and  
-          Computation <a className={styles.link} href="https://ec24.sigecom.org/program/accepted-papers/index.html" target="_blank" rel="noopener noreferrer">(EC'24)</a>
-          <br /> &nbsp;&nbsp;&nbsp;— Accepted into the 7th World Congress of the Game Theory 
+          <br /> &nbsp;&nbsp;&nbsp;&mdash; Accepted into the 25th ACM Conference on Economics and  
+          Computation <a className={styles.link} href="https://ec24.sigecom.org/program/accepted-papers/index.html" target="_blank" rel="noopener noreferrer">(EC&apos;24)</a>
+          <br /> &nbsp;&nbsp;&nbsp;&mdash; Accepted into the 7th World Congress of the Game Theory 
           Society <a className={styles.link} href="https://indico.pku.edu.cn/event/21/page/77-list-of-participants" target="_blank" rel="noopener noreferrer">(GAMES 2024)</a>
-          <br /> &nbsp;&nbsp;&nbsp;— Accepted into the Conference on Information System and Technology 
+          <br /> &nbsp;&nbsp;&nbsp;&mdash; Accepted into the Conference on Information System and Technology 
           Technology <a className={styles.link} href="https://sites.google.com/view/cist2024/program" target="_blank" rel="noopener noreferrer">(CIST 2024)</a>
         </li>
         <br />
