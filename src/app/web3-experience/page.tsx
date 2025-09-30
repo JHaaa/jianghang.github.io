4c2a18017a5dfeb82f4e3ee76b53fb1585e5e96d
Jiang Hang <jianghang@r-10-108-25-172.comp.nus.edu.sg>
2025-09-30 11:04:23 +0800

    update link

--- src/app/web3-experience/page.tsx
+++ src/app/web3-experience/page.tsx
@@ -15,19 +15,25 @@ const Web3ExperiencePage = () => {
                 <h3 className={styles.header}>Articles</h3>
                 <ul className={styles.customList}>
                 <li>
-                        Jiang, Hang. (2025). 
+                        Hang Jiang. (2025). 
                         <a className={styles.link} href="https://ethresear.ch/t/toward-a-general-model-for-proposer-selection-mechanism-design/21790" target="_blank" rel="noopener noreferrer">
                             &ldquo;Toward a General Model for Proposer Selection Mechanism Design,&rdquo; 
                         </a> <em>Ethereum Research</em>, Feb 2025.
                     </li>
                     <li>
-                        Jiang, Hang. (2022). 
+                        Hang Jiang. (2025). 
+                        <a className={styles.link} href="https://papers.ssrn.com/sol3/papers.cfm?abstract_id=4853229" target="_blank" rel="noopener noreferrer">
+                            &ldquo;A Dynamic Model for Token-gated Clubs,&rdquo; 
+                        </a> <em>preprint</em>, Aug 2022.
+                    </li>
+                    <li>
+                        Hang Jiang. (2022). 
                         <a className={styles.link} href="https://mirror.xyz/globalcoinresearch.eth/0oJnV52MRJ-bYCMz9vQnT3ZBDaGal5A0mJM9gzXIb8k" target="_blank" rel="noopener noreferrer">
                             &ldquo;How Much Liquidity is Needed to Solve Sandwich Attacks,&rdquo; 
                         </a> <em>Global Coin Research</em>, March 2022.
                     </li>
                     <li>
-                        Jiang, Hang. (2021). 
+                        Hang Jiang. (2021). 
                         <a className={styles.link} href="https://mirror.xyz/globalcoinresearch.eth/ZW0f3noANH-WEUg7GTYbt6Yk98PZA19tjp-XIHSv7pU" target="_blank" rel="noopener noreferrer">
                             &ldquo;What Does DAO 2.0 Tokenomics Look Like,&rdquo; 
                         </a> <em>Global Coin Research</em>, December 2021.
